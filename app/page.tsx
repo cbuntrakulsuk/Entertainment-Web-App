bbb305ab57d5d9a6d2a39724d579ad27dd8e8216
Chad Buntrakulsuk <chad.buntrakulsuk@gmail.com>
2024-03-24 23:27:01 -0700

    added navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex w-full h-full flex-col items-center justify-between">
-      <h1 className="">Entertianment Web App</h1>
-    </main>
+    <main className="flex w-full h-full flex-col items-center justify-between"></main>
   );
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,60 @@
 import React from "react";
+import Image from "next/image";
+
+// Assuming the image file is in the `public/assets` directory
+import Logo from "../public/assets/logo.svg";
+import Home from "../public/assets/icon-nav-home.svg";
+import Movies from "../public/assets/icon-nav-movies.svg";
+import TV from "../public/assets/icon-nav-tv-series.svg";
+import Bookmarks from "../public/assets/icon-nav-bookmark.svg";
+import Avatar from "../public/assets/image-avatar.png";
 
 const Navbar = () => {
-  return <div className="h-full m-8 w-24 bg-grayBlue">Navbar</div>;
+  return (
+    <>
+      <div className="h-screen m-8 w-24 bg-semiDarkBlue text-center rounded-2xl">
+        <div className="pt-8 pb-20 flex justify-center items-center">
+          <Image
+            src={Logo} // Path to your image
+            alt="Logo" // Descriptive alternative text
+          />
+        </div>
+        <div className="pb-10 flex justify-center items-center">
+          <Image
+            src={Home} // Path to your image
+            alt="Navigation icon for Home" // Descriptive alternative text
+          />
+        </div>
+        <div className="pb-10 flex justify-center items-center">
+          <Image
+            src={Movies} // Path to your image
+            alt="Navigation icon for Movies" // Descriptive alternative text
+          />
+        </div>
+        <div className="pb-10 flex justify-center items-center">
+          <Image
+            src={TV} // Path to your image
+            alt="Navigation icon for TV Series" // Descriptive alternative text
+          />
+        </div>
+        <div className="pb-10 flex justify-center items-center">
+          <Image
+            src={Bookmarks} // Path to your image
+            alt="Navigation icon for Bookmarks" // Descriptive alternative text
+          />
+        </div>
+
+        <div className="pb-10 flex justify-center items-center">
+          <Image
+            src={Avatar} // Path to your image
+            width={40}
+            height={40}
+            alt="Navigation icon for Avatar" // Descriptive alternative text
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
